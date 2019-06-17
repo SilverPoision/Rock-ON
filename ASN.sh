01f338d1af40ba5e93bc77aa36dddbfad624fa5e
Piyush kumar <piyush1671@gmail.com>
2019-06-17 09:29:19 +0530

    Update ASN.sh

--- ASN.sh
+++ ASN.sh
@@ -12,7 +12,7 @@ function asn_enum {
     sed "s/^[ \t]*//" n4$asn.txt > n5$asn.txt
     rm -f n1$asn.txt n2$asn.txt n3$asn.txt n4$asn.txt netblocks$asn.txt
     ./Tools/masscan/bin/masscan -p0-65535 -iL n5$asn.txt --rate 100000 -oG out$asn.txt
-    curl -X POST -H 'Content-type: applicion/json' --data '{"text":"Your ASN Enumeration process for '$asn' has been completed"}' https://hooks.slack.com/services/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
+    curl -X POST -H 'Content-type: applicion/json' --data '{"text":"Your ASN Enumeration process for '$asn' has been completed"}' https://hooks.slack.com/services/TKHQXXXXX/BK6CGP62X/PtV7fggkgVpXX3xxxxxXXXXX
 
 }
 
