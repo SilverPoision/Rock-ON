aef82203defc4660748baacdde9d5bd2ecafa957
Piyush kumar <piyush1671@gmail.com>
2019-06-17 14:14:22 +0530

    Update sub.sh

--- sub.sh
+++ sub.sh
@@ -21,7 +21,7 @@ read S_key
 echo "+--------------------------------Subdomains By Sublister-------------------------------------+"
 
 python Tools/Sublist3r/sublist3r.py -d $domain -o $domain/sublister$domain.txt
-echo -e \n\n "${RED}Sublister Ended..${NC}"
+echo -e  "\n\n ${RED}Sublister Ended..${NC}"
 
 #Running Knock
 echo "+--------------------------------Subdomains By Knock-------------------------------------+"
@@ -32,7 +32,7 @@ myknock=$(echo $(ls -t |head -n1))
 #Stripping out the domain coloum to the domain.txt
 cut -d , -f 4 $myknock >> $domain/knockpy$domain.txt
 rm -f $myknock
-echo -e \n\n "${RED}Knock Ended..${NC}"
+echo -e "\n\n ${RED}Knock Ended..${NC}"
 
 #Running Sublert
 echo "+--------------------------------Subdomains By Sublert-------------------------------------+"
@@ -40,34 +40,34 @@ cd Tools/sublert
 python sublert.py -u $domain
 cd ..
 cd ..
-echo \n\n -e "${RED}Sublert Ended..${NC}"
+echo -e "\n\n ${RED}Sublert Ended..${NC}"
 
 #Running Censys 
 echo "+--------------------------------Subdomains By Censys-------------------------------------+"
 export CENSYS_API_ID=$API_key
 export CENSYS_API_SECRET=$S_key
 python Tools/censys-subdomain-finder/censys_subdomain_finder.py $domain -o $domain/censys$domain.txt
-echo -e \n\n "${RED}Censys Ended..${NC}"
+echo -e "\n\n ${RED}Censys Ended..${NC}"
 
 #Running Amass
 echo "+--------------------------------Subdomains By Amass-------------------------------------+"
 ./Tools/amass enum -passive -d $domain -o $domain/amass$domain.txt
-echo -e \n\n "${RED}Amass Ended..${NC}"
+echo -e "\n\n ${RED}Amass Ended..${NC}"
 
 #Running SubFinder
 echo "+--------------------------------Subdomains By SubFinder-------------------------------------+"
 ./Tools/subfinder/subfinder -d $domain -b -w Tools/my.txt -o $domain/subfinder$domain.txt
-echo -e \n\n "${RED}Subfinder Ended..${NC}"
+echo -e "\n\n ${RED}Subfinder Ended..${NC}"
 
 #Running CT
 echo "+--------------------------------Subdomains By CT-------------------------------------+"
 python Tools/bugcrowd-levelup-subdomain-enumeration/subdomain_enum_crtsh.py --domains $domain >> $domain/ct$domain.txt
-echo -e \n\n "${RED}CT Ended..${NC}"
+echo -e "\n\n ${RED}CT Ended..${NC}"
 
 #Running CTFR
 echo "+--------------------------------Subdomains By CTFR-------------------------------------+"
 python3 Tools/ctfr/ctfr.py -d $domain -o $domain/ctfr$domain.txt
-echo -e \n\n "${RED}CTFR Ended..${NC}"
+echo -e "\n\n ${RED}CTFR Ended..${NC}"
 
 #Running CTFR
 echo "+--------------------------------Subdomains By Aquatone-------------------------------------+"
@@ -78,29 +78,29 @@ echo -e "${RED}Aquatone Ended..${NC}"
 #Running Wayback
 echo "+--------------------------------Subdomains By Wayback-------------------------------------+"
 curl -s "http://web.archive.org/cdx/search/cdx?url=*.$domain/*&output=text&fl=original&collapse=urlkey" |sort| sed -e 's_https*://__' -e "s/\/.*//" -e 's/:.*//' -e 's/^www\.//' | uniq >> $domain/wayback$domain.txt
-echo -e \n\n "${RED}Wayback Ended..${NC}"
+echo -e "\n\n ${RED}Wayback Ended..${NC}"
 
 #Running San
 echo "+--------------------------------Subdomains By San-------------------------------------+"
 python Tools/San/san.py $domain >> $domain/san$domain.txt
-echo -e \n\n "${RED}San Ended..${NC}"
+echo -e "\n\n ${RED}San Ended..${NC}"
 
 #Running Vhost
 echo "+--------------------------------Subdomains By VHost-------------------------------------+"
 nmap --script http-vhosts -p 80,8080,443 $domain > $domain/vhost$domain.txt
-echo -e \n\n "${RED}Vhost Ended..${NC}"
+echo -e "\n\n ${RED}Vhost Ended..${NC}"
 
 #Running FDNS
 echo "+--------------------------------Subdomains By Rapid7 FDNS-------------------------------------+"
 cat Tools/2019-05-31-1559261131-fdns_cname.json.gz | pigz -dc | grep -E "\.$domain" | jq .name | uniq > $domain/fdns$domain.txt
 sed 's/\"//g' $domain/fdns$domain.txt > $domain/fdns_new$domain.txt
-echo -e \n\n "${RED}Rapid7 FDSN Ended..${NC}"
+echo -e "\n\n ${RED}Rapid7 FDSN Ended..${NC}"
 
 #Running MassDNS
 echo "+--------------------------------Subdomains By MassDNS-------------------------------------+"
 python Tools/massdns/scripts/subbrute.py Tools/massdns/all.txt $domain | ./Tools/massdns/bin/massdns -r Tools/massdns/lists/resolvers.txt -t A -o S -w $domain/massdns$domain.txt
 sed 's/A.*//' $domain/massdns$domain.txt | sed 's/CN.*//' | sed 's/\..$//' > $domain/massdns1$domain.txt 
-echo -e \n\n "${RED}MassDNS Ended..${NC}"
+echo -e "\n\n ${RED}MassDNS Ended..${NC}"
 
 echo "Combining all the Files"
 cd $domain
